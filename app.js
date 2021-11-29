c10acda7ea963d78bb82f9cc05d88c3bc02e3053
Daria Trip <daria.trip001@gmail.com>
2021-11-29 11:41:41 +0200

    exersare comenzi html/css

---
+++ app.js
@@ -0,0 +1,13 @@
+//let a = document.getElementById("button");
+
+
+    /*let a = document.getElementsById("button");
+
+    a.addEventListener("click", alertMeessage);
+
+    function alertMeessage() {
+       alert("You have clicked the button");
+    }*/
+
+
+alert("this is an alert!");
