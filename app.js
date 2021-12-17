13e01aac6fe2ab6191f43d06db726242377781da
Daria Trip <daria.trip001@gmail.com>
2021-12-17 20:34:40 +0200

    Form 1 - js part

--- app.js
+++ app.js
@@ -1,97 +1,56 @@
-
-//Am rezolvat problema cu butonul de alert
-//puteti testa pe site 
-
-/*Nu uitati sa stergeti continutul acestui fisier*/
-
-/*function alertMessage() {
-    alert("You have clicked the button!");
-}*/
-
-
-
+function radio_checked(){
+  var set1 = new RadioGroup(document.getElementById('set1-of-buttons'));
+  rg1.init();
+  var set2 = new RadioGroup(document.getElementById('set2-of-buttons'));
+  rg2.init();
+}
 
 function calsPerDay() {
-    function find(id) {
-      return document.getElementById(id);
-    } 
-    var age = find("age").value;
-    var height = find("height").value;
-    var weight = find("weight").value;
-    var result = 0;
-    if (find("male").checked) result = 10 * weight + 6.25 * height - 5 * age + 5;
-    else if (find("female").checked) result = 10 * weight + 6.25 * height - 5 * age - 161;
-    find("totalCals").innerHTML = Math.round(result);
-  }
+    var age = parseInt(document.getElementById("age").value);
+    var height = parseInt(document.getElementById("height").value);
+    var weight = parseInt(document.getElementById("weight").value);
+    var male= document.getElementById("male");
+    var female = document.getElementById("female");
 
-  //ceva de genul trebuie facut pt a afisa in elementul h1 
-  //document.getElementById("output_result1").innerHTML = result;
-  //calsPerDay();
+    if (male.checked)
+      result = (10 * weight) + (6.25 * height) - (5 * age) + 5;
+    else if (female.checked) 
+      result = (10 * weight) + (6.25 * height) - (5 * age) - 161;
+      
+    activityLevel(result);
+} 
+function activityLevel(result)
+{
+  var activity1 = document.getElementById("activity1");
+  var activity2 = document.getElementById("activity2");
+  var activity3 = document.getElementById("activity3");
+  var activity4 = document.getElementById("activity4");
+  var activity5 = document.getElementById("activity5");
+  if(activity1.selected)
+  result *= 1.2;
+  else if(activity2.selected)
+  result *=1.375;
+  else if(activity3.selected)
+  result *=1.55;
+  else if(activity4.selected)
+  result *=1.725;
+  else if(activity5.selected)
+  result *=1.9;
+  
+  goal_checked(result);
+}
 
- 
- 
- 
- 
- 
- 
- 
-  /*(function() {
-    const form    = document.getElementById('calc-form');
-    const results = document.getElementById('results');
-    const errors  = document.getElementById('form-error');
-    function errorMessage(msg) {
-        errors.innerHTML = msg;
-        errors.style.display = '';
-        return false;
-    }
-    function showResults(calories) {
-        results.innerHTML = `<p>Your basal metabolic rate (BMR) is: <strong>${(calories).toFixed(2)} </strong> calories a day.</p><a href="#" id="rs">revise</a>`;
-      results.style.display = ''
-      form.style.display = 'none'
-      errors.style.display = 'none'
-    }
-    function resetForm(e) {
-      if(e.target.id = 'rs') {
-        e.preventDefault();
-        results.style.display = 'none';
-        form.style.display = '';
-        form.reset()
-      }
-    }
-    function submitHandler(e) {
-        e.preventDefault();
-        let age = parseFloat(form.age.value);
-        if(isNaN(age) || age < 0) {
-            return errorMessage('Please enter a valid age');
-        }
-        let heightCM = parseFloat(form.height_cm.value);
-        if(isNaN(heightCM) || heightCM < 0) {
-            
-          let heightFeet = parseFloat(form.height_ft.value);
-          if(isNaN(heightFeet) || heightFeet < 0) {
-              return errorMessage('Please enter a valid Height in feet or centimeters');
-          }      
-         let heightInches = parseFloat(form.height_in.value);
-          if(isNaN(heightInches) || heightInches < 0) {
-              heightInches=0;
-          }   
-          heightCM = (2.54 * heightInches) + (30.4 * heightFeet)        
-        }   
-          let weight = parseFloat(form.weight.value);
-          if(isNaN(weight) || weight < 0) {
-              return errorMessage('Please enter a valid weight');
-          }   
-        if(form.weight_unit.value == 'lb') {
-            weight = 0.453592 * weight;
-        }     
-       let calories = 0;
-       if(form.gender.value == 'Female') { 
-         calories = 655.09 + (9.56 * weight) + (1.84 * heightCM) - (4.67 * age);
-        }  else {
-         calories = 66.47 + (13.75 * weight) + (5 * heightCM) - (6.75 * age);
-        }
-       showResults(calories);
-    }
-    form.addEventListener('submit', submitHandler);
-    results.addEventListener('click', resetForm, true);
-})();*/
+
+function goal_checked(result)
+{
+  var fat_loss=document.getElementById("fat_loss");
+  var muscle_gain=document.getElementById("muscle_gain");
+
+  if(fat_loss.selected)
+    result *= 0.75;
+  else if(muscle_gain.selected)
+    result *= 1.25;
+
+  //Rezultat FINAL
+  document.getElementById("output_result1").innerHTML = Math.round(result);
+}
